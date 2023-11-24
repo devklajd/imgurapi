ae18dc4dcce7a8e3d844a41c896f77f9daeaf700
Klajd Belishaku <145049182+devklajd@users.noreply.github.com>
2023-11-24 15:26:03 +0100

    Update gallery.js

--- backend/routes/gallery.js
+++ backend/routes/gallery.js
@@ -1,48 +1,55 @@
 const express = require("express");
 const axios = require("axios");
-const Constants = require('../constants/constants');
 
 const router = express.Router();
 
+const CLIENT_ID = '443cc174f434907';  // Your Imgur client ID
+
 router.get('/:id', (req, res, next) => {
-    let postId = req.params.id,
-        url = 'https://api.imgur.com/3/gallery/album/' + postId;
+    let postId = req.params.id;
+    let url = `https://api.imgur.com/3/gallery/album/${postId}`;
     console.log(url);
+
     axios({
         method: 'get',
         url: url,
-        headers: Constants["CLIENT_ID"]
+        headers: {
+            'Authorization': 'Client-ID ' + CLIENT_ID
+        }
     }).then(function (response) {
         console.log(response.data);
-        res.status(200).send(response.data);
+        if (response.data) {
+            res.status(200).send(response.data);
+        } else {
+            res.status(404).json({ error: "Data not found" });
+        }
     }).catch(function (error) {
         console.log(error);
-        res.status(404).json({ error: "rerer" });
+        res.status(500).json({ error: "Internal Server Error" });
     });
-    //res.status(200).send({ "response": "hai" });
-    // https://api.imgur.com/3/gallery/image/{id}
 });
 
 router.post('', (req, res, next) => {
-
     let { section, sort, showViral, window } = req.body;
-
-    // console.log(req);
-
     let url = `https://api.imgur.com/3/gallery/${section}/${sort}/${window}/0?showViral=${showViral}`;
-
     console.log(url);
 
     axios({
         method: 'get',
         url: url,
-        headers: Constants["CLIENT_ID"]
+        headers: {
+            'Authorization': 'Client-ID ' + CLIENT_ID
+        }
     }).then(function (response) {
-        res.status(200).send(response.data);
+        if (response.data) {
+            res.status(200).send(response.data);
+        } else {
+            res.status(404).json({ error: "Data not found" });
+        }
     }).catch(function (error) {
         console.log(error);
+        res.status(500).json({ error: "Internal Server Error" });
     });
-
 });
 
 module.exports = router;
