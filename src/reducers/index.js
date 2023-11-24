b75c044310d455192295d3e3c6b0c5cb35dbf33b
Klajd Belishaku <145049182+devklajd@users.noreply.github.com>
2023-11-24 19:35:17 +0100

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,39 +13,34 @@ const initialState = {
     window: "day",
     showViral: true,
     post: {}
-
 }
 
 const reducer = (state = initialState, action) => {
-
     switch (action.type) {
-
         case types.GET_GALLERY_CONTENT:
-            return { ...state, gallery: action.payload.body.data }
+            return { ...state, gallery: action.payload.body.data };
 
         case types.SET_SECTION:
-            return { ...state, section: action.payload.section }
+            return { ...state, section: action.payload.section };
 
         case types.SET_VIRAL_IMAGE_VISIBLITY:
-            return { ...state, showViral: action.payload.visiblity }
+            return { ...state, showViral: action.payload.visiblity };
 
         case types.SET_SORT:
-            return { ...state, sort: action.payload.sort }
+            return { ...state, sort: action.payload.sort };
 
         case types.SET_WINDOW:
-            return { ...state, window: action.payload.window }
+            return { ...state, window: action.payload.window };
 
         case types.GET_SINGLE_POST:
-            return { ...state, post: action.payload.body.data }
+            return { ...state, post: action.payload.body.data };
 
         case types.CLEAR_POST:
-            return { ...state, post: action.payload.post }
+            return { ...state, post: action.payload.post };
 
         default:
-            break;
+            return state;
     }
-    return state;
 };
-
 
 export default reducer;
